0e455755fe9a4b0156dd6116213cf94146b4ad51
James Cahill <jcahill773@gmail.com>
2023-03-15 12:16:43 +0000

    (constants): Adjust commment positions

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,9 @@
 export const Constants = {
 	// TinyUSB Defaults
 	USB_VENDOR_ID: 0xcafe,
-	USB_PRODUCT_ID: 0x4011, // generated based on settings,
+	USB_PRODUCT_ID: 0x4011,
 
+	// generated based on settings
 	ENDPOINT: 3,
 
 	// 'magic' bytes that identifies the start of a bulk packet.
