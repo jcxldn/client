1fa36f95a9febbefd999bde27f9bf50d0e72c3c7
James Cahill <jcahill773@gmail.com>
2023-03-15 12:11:26 +0000

    (constants): Run prettier linting

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const Constants = {
-    // TinyUSB Defaults
-    USB_VENDOR_ID: 0xcafe,
-    USB_PRODUCT_ID: 0x4011, // generated based on settings,
+	// TinyUSB Defaults
+	USB_VENDOR_ID: 0xcafe,
+	USB_PRODUCT_ID: 0x4011, // generated based on settings,
 
-    ENDPOINT: 3,
-}
+	ENDPOINT: 3,
+};
