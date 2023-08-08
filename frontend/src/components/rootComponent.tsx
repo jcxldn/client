000faa511cac8a9a2985590986f3f46b0f0ee8e2
James Cahill <jcahill773@gmail.com>
2023-08-08 08:43:06 +0100

    (frontend): Initial root page with connect button

---
+++ frontend/src/components/rootComponent.tsx
@@ -0,0 +1,56 @@
+import * as React from "react";
+
+import { Client } from "backend/src/index"
+
+import { Button } from "@mui/material";
+
+export default class RootComponent extends React.Component {
+    client: Client;
+
+    state = {
+        hasDevice: false
+    }
+
+    constructor(props: any) {
+        super(props);
+
+        this.client = new Client();
+    }
+
+    private async updateHasDeviceState(): Promise<void> {
+        return this.setState({ hasDevice: await this.client.hasDevice() })
+    }
+
+    async componentDidMount(): Promise<void> {
+        await this.client.workerCreateClient();
+        await this.updateHasDeviceState();
+    }
+
+    private async handleConnect() {
+        await this.client.requestDevice();
+
+        if (await this.client.hasDevice()) {
+            // We have a device now, let's open it!
+            await this.client.workerOpenDevice();
+
+            await this.client.workerFindInterface();
+
+            await this.client.workerClaimInterface();
+        }
+
+        await this.updateHasDeviceState();
+    }
+
+    private handleDisconnect() {
+        console.log("disconnectCalled");
+    }
+
+    render() {
+        return (
+            <>
+                {!this.state.hasDevice ? <Button variant="outlined" onClick={() => this.handleConnect()}>Connect</Button> : <Button variant="contained" onClick={() => this.handleDisconnect()}>Disconnect</Button>}
+            </>
+        )
+
+    }
+}
